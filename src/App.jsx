f803a38adeac56cd05305160d12e7c839c2f4574
tcormons-verys <tomas.cormons@verys.com>
2021-04-09 15:03:31 -0500

    meets basic requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
-import cntl from "cntl";
+// import cntl from "cntl";
 import Button from "./stories/Components/Button/Button";
 import CollapsibleSection from "./stories/Components/CollapsibleSection/CollapsibleSection";
 import Input from "./stories/Components/Input/Input";
@@ -8,59 +8,201 @@ import Dropdown from "./stories/Components/Dropdown/Dropdown";
 import ProgressTracker from "./stories/Components/ProgressTracker/ProgressTracker";
 import NavBar from "./stories/Components/NavBar/NavBar";
 
-const containerCN = cntl`
-  mt-3
-  p-3
-  border
-  rounded
-`;
+const reducer = (state, action) => {
+  switch (action.parentKey) {
+    case "owner":
+      return {
+        ...state,
+        owner: { ...state.owner, [action.key]: action.newValue },
+      };
+    case "address":
+      return {
+        ...state,
+        address: { ...state.address, [action.key]: action.newValue },
+      };
+    default:
+      return {
+        ...state,
+        [action.key]: action.newValue,
+      };
+  }
+};
+
+const steps = ["Client Info", "Logo", "Branding", "App Store"];
+
+const dropDownSteps = [
+  { label: "Client Info", value: "Client Info" },
+  { label: "Client Info", value: "Client Info" },
+  { label: "Client Info", value: "Client Info" },
+  { label: "Client Info", value: "Client Info" },
+];
 
 const App = () => {
+  const [object, setObject] = useReducer(reducer, {
+    name: "",
+    eSpace: "",
+    subscription: {},
+    owner: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+    address: {
+      street: "",
+      unit: "",
+      city: "",
+      country: "",
+      zipCode: "",
+    },
+  });
+
+  const handleInputChange = (newValue, key, parentKey) => {
+    setObject({ key, newValue, parentKey });
+  };
+
+  const handleDropdownChange = (newValue, key, parentKey) => {
+    setObject({ key, newValue, parentKey });
+  };
+
+  const handleSave = () => {
+    console.log({ object });
+  };
+
   return (
-    <div className="bg-black h-full w-full flex justify-center">
-      <div className="p-10 w-1/3">
-        <p>Create a screen here!</p>
-        <p>
-          Below are the provided Components that will be needed. You won&apos;t
-          be expected to modify these Components at all, but you may need to
-          study them and pass props.
-        </p>
-        <div className={containerCN}>
-          <p>Nav bar</p>
-          <NavBar />
-        </div>
-        <div className={containerCN}>
-          <p>Progress Tracker</p>
-          <ProgressTracker
-            steps={Array(5)
-              .fill()
-              .map((a, index) => `Step ${index + 1}`)}
-          />
-        </div>
-        <div className={containerCN}>
-          <p>Collapsible Section</p>
-          <CollapsibleSection title="Section Title">
-            <p>inner content</p>
-          </CollapsibleSection>
-        </div>
-        <div className={containerCN}>
-          <p>Button</p>
-          <Button title="Button" />
-        </div>
-        <div className={containerCN}>
-          <p>Input</p>
-          <Input label="Label" placeholder="placeholder" />
-        </div>
-        <div className={containerCN}>
-          <p>Dropdown</p>
-          <Dropdown
-            options={Array(5)
-              .fill()
-              .map((a, index) => ({
-                label: `Option ${index + 1}`,
-                value: `Option ${index + 1}`,
-              }))}
-          />
+    <div className="bg-black h-full w-full flex justify-center overflow-auto">
+      <div className="w-full">
+        <NavBar />
+        <div className="p-10">
+          <div className="p-10">
+            ADD NEW CLIENT
+            <ProgressTracker steps={steps} />
+          </div>
+          <div>
+            <CollapsibleSection title="Overview">
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Company Name"
+                  placeholder="Company Name"
+                  value={object.name}
+                  onChange={(newValue) => handleInputChange(newValue, "name")}
+                />
+                <Input
+                  className="w-full p-5"
+                  label="eSpace Name*"
+                  placeholder="eSpace Name"
+                  value={object.eSpace}
+                  onChange={(newValue) => handleInputChange(newValue, "eSpace")}
+                />
+              </div>
+              <div className="flex justify-between">
+                <Dropdown
+                  className="w-1/2 p-5"
+                  label="Subscription*"
+                  value={object.subscription}
+                  options={dropDownSteps}
+                  onChange={(newValue) =>
+                    handleDropdownChange(newValue, "subscription")
+                  }
+                />
+              </div>
+            </CollapsibleSection>
+          </div>
+          <hr />
+          <div>
+            <CollapsibleSection title="Owner Information">
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Primary Owner"
+                  placeholder="Primary Owner"
+                  value={object.owner.name}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "name", "owner")
+                  }
+                />
+                <Input
+                  className="w-full p-5"
+                  label="Primary Owner Email"
+                  placeholder="Primary Owner Email"
+                  value={object.owner.email}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "email", "owner")
+                  }
+                />
+              </div>
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Primary Owner Phone"
+                  placeholder="Primary Owner Phone"
+                  value={object.owner.phone}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "phone", "owner")
+                  }
+                />
+              </div>
+            </CollapsibleSection>
+          </div>
+          <hr />
+          <div>
+            <CollapsibleSection title="Location Information">
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Street Address*"
+                  placeholder="Street Address"
+                  value={object.address.street}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "street", "address")
+                  }
+                />
+                <Input
+                  className="w-full p-5"
+                  label="City"
+                  placeholder="City"
+                  value={object.address.city}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "city", "address")
+                  }
+                />
+              </div>
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Suit/Unit"
+                  placeholder="Suit/Unit"
+                  value={object.address.unit}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "unit", "address")
+                  }
+                />
+                <Dropdown
+                  className="w-full p-5"
+                  label="Country"
+                  options={dropDownSteps}
+                  value={object.address.country}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "country", "address")
+                  }
+                />
+              </div>
+              <div className="flex justify-between">
+                <Input
+                  className="w-full p-5"
+                  label="Postal Code"
+                  placeholder="Postal Code"
+                  value={object.address.zipCode}
+                  onChange={(newValue) =>
+                    handleInputChange(newValue, "zipCode", "address")
+                  }
+                />
+              </div>
+            </CollapsibleSection>
+          </div>
+          <div className="p-10">
+            <Button title="Save" onClick={handleSave} />
+          </div>
         </div>
       </div>
     </div>
